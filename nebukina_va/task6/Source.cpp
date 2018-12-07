73e28879af17f574f399d459a8905ad11085fb14
NebukinaVA <43473978+NebukinaVA@users.noreply.github.com>
2018-12-07 10:38:10 +0300

    Update Source.cpp

--- nebukina_va/task6/Source.cpp
+++ nebukina_va/task6/Source.cpp
@@ -5,33 +5,53 @@
 #include <locale.h>
 #include <math.h>
 
-void Sin(long double x, long double accuracy, long N)
+double F(long n)
 {
-	long double y = 0;
-	long i, j;
-	for (i = 0; i <= N; i++)
+	long i;
+	long fact = 1;
+	for (i = 1; i <= n; i++)
 	{
-		y = y + ((-1)^i)*(x ^ (2 * i + 1)) / ((2 * i + 1)!); // изменить тип х
+		fact *= i;
 	}
 }
 
-void Cos(long double x, long double accuracy, long N)
+double mysin(long double x, long double accuracy, long N)
 {
 	long double y = 0;
 	long i, j;
 	for (i = 0; i <= N; i++)
 	{
-		y = y + ((-1) ^ i)*(x ^ (2 * i)) / ((2 * i)!); // изменить тип х
+		y = y + ((-1) ^ i)*(pow(x, (2 * i + 1))) / (F(2 * i + 1));
 	}
 }
 
-void Exp(long double x, long double accuracy, long N)
+double mycos(long double x, long double accuracy, long N)
 {
 	long double y = 0;
 	long i, j;
 	for (i = 0; i <= N; i++)
 	{
-		y = y + (x ^ i) / (i!); // изменить тип х
+		y = y + ((-1) ^ i)*(pow(x, (2 * i))) / (F(2 * i)); 
+	}
+}
+
+double myexp(long double x, long double accuracy, long N)
+{
+	long double y = 0;
+	long i, j;
+	for (i = 0; i <= N; i++)
+	{
+		y = y + (pow(x, i)) / (F(i)); 
+	}
+}
+
+double myarsh(long double x, long double accuracy, long N)
+{
+        long double y = 0;
+	long i;
+	for (i = 0; i <= N; i++)
+	{
+
 	}
 }
 
@@ -39,7 +59,7 @@ int main()
 {
 	double x;
 	long double accuracy, etalon, myresult;
-	unsigned long double delta;
+	long double delta;
 	long N = 0;
 	int funct = 0;
 	int mode = 0;
@@ -54,24 +74,23 @@ int main()
 	{
 		while ((funct < 1) || (funct > 4))
 		{
-		ptintf("Выберите функцию из предложенных:\n1) sin(x)\n2) cos(x)\n3) exp(x)\n4) arsh(x)\n");
+		printf("Выберите функцию из предложенных:\n1) sin(x)\n2) cos(x)\n3) exp(x)\n4) arsh(x)\n");
 		scanf_s("%i", &funct);
 		}
 		printf("Введите точку x, в которой необходимо вычислить значение.\n");
-		scanf_s("%d", &x);
-		printf("Введите желаемую точность результата (0.000001 или больше).\n");
-		scanf_s("%d", &accuracy);
+		scanf_s("%f", &x);
+		printf("Введите желаемую точность результата (0,000001 или больше).\n");
+		scanf_s("%f", &accuracy);
 		while ((N < 1) || (N > 1000))
 		{
-			ptintf("Задайте число элементов ряда для выполнения расчета (от 1 до 1000):\n");
-			scanf_s("%l", &N);
+			printf("Задайте число элементов ряда для выполнения расчета (от 1 до 1000):\n");
+			scanf_s("%d", &N);
 		}
 
 		switch (funct)
 		{
 		case 1:
 			etalon = sin(x);
-			myresult = Sin(x, accuracy, N);
 		case 2:
 			etalon = cos(x);
 		case 3:
@@ -79,9 +98,9 @@ int main()
 		case 4:
 			etalon = arsh(x);
 		}
-		delta = | etalon - myresult | ;
-		printf("Эталонное значение в точке x: %d", etalon);
-		printf("Вычисленная оценка значения функции в точке x: %d", myresult);
-		printf("Разница между оценкой и эталонным значением в точке x: %ud", delta);
+		delta = etalon - myresult; // по модулю
+		printf("Эталонное значение в точке x: %f", etalon);
+		printf("Вычисленная оценка значения функции в точке x: %f", myresult);
+		printf("Разница между оценкой и эталонным значением в точке x: %fu", delta);
 	}
 }
